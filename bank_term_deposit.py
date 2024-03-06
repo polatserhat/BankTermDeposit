68aac17e97b37c9ef44daff5d5534509692f6981
polatserhat <132306641+polatserhat@users.noreply.github.com>
2024-03-06 09:09:13 +0300

    Delete bank_term_deposit.py

--- bank_term_deposit.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-test_data = pd.read_csv("data/test.csv")
-train_data = pd.read_csv("data/train.csv")
-
-# print test data
-print(f'\nTest Data:\n\n\n{test_data}\n\n\n\n')
-#print train data
-print(f'Train Data:\n\n\n{train_data}')
